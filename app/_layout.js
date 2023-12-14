82c5a59e64c6db92b1c5afe81f45293bcf62df87
Sooraj B <sooraj.b@webandcrafts.com>
2023-12-14 23:54:26 +0530

    initial setup, custom fonts setup complete

---
+++ app/_layout.js
@@ -0,0 +1,34 @@
+import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from "react";
+
+SplashScreen.preventAutoHideAsync();
+
+const Layout = () => {
+  const fonts = {
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  };
+
+  const [fontsLoaded, fontError] = useFonts(fonts);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+  return <Stack onLayout={onLayoutRootView} />;
+};
+
+export default Layout;
